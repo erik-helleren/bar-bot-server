34190e112c2523cfdc3990b14a500c1c31a8f399
erik-helleren <erik.helleren@gmail.com>
2014-02-21 10:43:29 -0500

    Pump test
    I set up a basic pump test to test any formula/logic used to calculate
    the amount of fluid dispensed from a pump.

--- relayTest/relayTest.ino
+++ relayTest/relayTest.ino
@@ -7,25 +7,31 @@
 #define relay7 41
 #define relay8 39
 
+#define ml_per_sec 33
+#define ml_offset 7
+
 void setup(){
- pinMode(relay1,OUTPUT);
- pinMode(relay2,OUTPUT);
- pinMode(relay3,OUTPUT);
- pinMode(relay4,OUTPUT);
- pinMode(relay5,OUTPUT);
- pinMode(relay6,OUTPUT);
- pinMode(relay7,OUTPUT);
- pinMode(relay8,OUTPUT);
-   digitalWrite(relay1,HIGH);
+  pinMode(relay1,OUTPUT);
+  pinMode(relay2,OUTPUT);
+  pinMode(relay3,OUTPUT);
+  pinMode(relay4,OUTPUT);
+  pinMode(relay5,OUTPUT);
+  pinMode(relay6,OUTPUT);
+  pinMode(relay7,OUTPUT);
+  pinMode(relay8,OUTPUT);
+  digitalWrite(relay1,HIGH);
   delay(10000);
   digitalWrite(relay1,LOW);
   delay(10000);
 }
+int mL_target=10;
 void loop(){
-
-  
+  long startTime;
+  for(int i=0;i<3;i++){
+    startTime=millis();
     digitalWrite(relay1,HIGH);
-  delay(10000);
-  digitalWrite(relay1,LOW);
-  delay(20000);
+    while( (millis()-startTime)*ml_per_sec-ml_offset <mL_target );
+    digitalWrite(relay1,LOW);
+    delay(10000);
+  }
 }
