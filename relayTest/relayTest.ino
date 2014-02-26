5bc2f783ab068b325a2365bf827852ef874cd3f7
Erik Helleren <erik.helleren@gmail.com>
2014-02-26 16:26:06 -0500

    Changed relayTest to test volumes
    I implemented the same logic in the main server, to test the pumps and
    snesors that we have and to make sure we get an accurately dispensed
    volume.

--- relayTest/relayTest.ino
+++ relayTest/relayTest.ino
@@ -1,3 +1,5 @@
+#include <TimerThree.h>
+
 #define relay1 53
 #define relay2 51
 #define relay3 49
@@ -7,8 +9,13 @@
 #define relay7 41
 #define relay8 39
 
+#define sensor1 22
+#define vout1 23
+
 #define ml_per_sec 33
 #define ml_offset 7
+
+
 
 void setup(){
   pinMode(relay1,OUTPUT);
@@ -19,19 +26,49 @@ void setup(){
   pinMode(relay6,OUTPUT);
   pinMode(relay7,OUTPUT);
   pinMode(relay8,OUTPUT);
-  digitalWrite(relay1,HIGH);
-  delay(10000);
-  digitalWrite(relay1,LOW);
-  delay(10000);
+  pinMode(relay8,OUTPUT);
+  pinMode(relay8,OUTPUT);
+  pinMode(sensor1,INPUT);
+  pinMode(vout1,OUTPUT);
+  long desiredFrequency=ml_per_sec;//Set frequency to be ml/sec so
+    //the interrupt calls has a 1:1 ratio of calls to ml dispensed
+  int microSeconds=(1000000/desiredFrequency);
+  Timer3.initialize(microSeconds);
+  Timer3.attachInterrupt(timedInterupt,microSeconds);
+  
 }
 int mL_target=10;
+int mL_poured=0;
+int delayCounter=0;
+int iterationCount=0;
 void loop(){
-  long startTime;
-  for(int i=0;i<3;i++){
-    startTime=millis();
-    digitalWrite(relay1,HIGH);
-    while( (millis()-startTime)*ml_per_sec-ml_offset <mL_target );
-    digitalWrite(relay1,LOW);
-    delay(10000);
+  
+}
+
+
+void timedInterupt(){
+  if(delayCounter>0){
+    delayCounter--;
+    return;
+  }
+  if(mL_poured<mL_target){
+    digitalWrite(vout1,HIGH);
+    if(digitalRead(sensor1)==HIGH){//if the sensor says there is fluid in the hose
+      mL_poured++;
+    }
+    digitalWrite(vout1,LOW);
+    if(mL_poured<mL_target){//if there is volume left
+      digitalWrite(relay1,HIGH);//turn on the pump
+    }else{//if there isn't any volume left
+      digitalWrite(relay1,LOW);//make sure the pump is off
+    }
+  }else{
+    digitalWrite(relay1,LOW);//Really make sure its off
+    mL_poured=0;
+    if(iterationCount++>=3){
+      iterationCount=0;
+      mL_target+=5;
+    }
+    delayCounter=10*ml_per_sec;//give a ten second pause after each measurement
   }
 }
